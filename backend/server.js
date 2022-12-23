73625d2883ef6aae2146064a8f360d8e743e5367
Manav-Mistry <msmistry07@gmail.com>
2022-12-23 10:30:26 +0530

    initial cimmit

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const dotenv = require("dotenv").config()
+const PORT = process.env.PORT || 5000
+// const cors = require("cors")
+// const morgan = require('morgan');
+const connectDB = require("./config/db")
+
+
+// connect to the database
+connectDB()
+
+const app = express()
+// app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use("/expenseBuddy/users", require("./routes/userRoutes"))
+
+app.listen(PORT, () => "server started...")
